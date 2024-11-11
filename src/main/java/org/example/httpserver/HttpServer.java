916ed990d7823a6056a569dabad9c6b0454a0d19
Tom-Jank <tom.jank4@gmail.com>
2024-11-11 23:14:09 +0100

    return proper http response

--- src/main/java/org/example/httpserver/HttpServer.java
+++ src/main/java/org/example/httpserver/HttpServer.java
@@ -1,6 +1,9 @@
 package org.example.httpserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 
 public record HttpServer(int portNumber) {
@@ -8,9 +11,14 @@ public record HttpServer(int portNumber) {
   public void serve() {
     try (
         var serverSocket = new ServerSocket(portNumber);
-        var socket = serverSocket.accept()
+        var clientSocket = serverSocket.accept();
+        var out = new PrintWriter(clientSocket.getOutputStream(), true);
+        var in = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()))
     ) {
-      System.out.println("Listening on port: " + this.portNumber);
+      out.write("HTTP/1.1 200 OK\r\n");
+      out.write("\r\n");
+      out.flush();
     } catch (IOException e) {
       System.out.println("Failed to start the server: " + e.getMessage());
     }
