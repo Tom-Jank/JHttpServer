f07011640c518de05579a19ddeafdfbf806be1cf
Tom-Jank <tom.jank4@gmail.com>
2024-11-14 12:36:36 +0100

    modified the server to accept multiple connections

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,12 @@ package org.example;
 import org.example.httpserver.HttpServer;
 
 /**
- * The goal of this application is to serve as a simple http server
- * with support for multiple connections and basic request types like GET.
+ * The goal of this application is to serve as a simple http server with support for multiple
+ * connections and basic request types like GET.
  */
 public class Main {
   public static void main(String[] args) {
     var server = new HttpServer(8080);
-    server.serve();
+    server.listen();
   }
 }
--- src/main/java/org/example/httpserver/HttpServer.java
+++ src/main/java/org/example/httpserver/HttpServer.java
@@ -5,22 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public record HttpServer(int portNumber) {
 
-  public void serve() {
-    try (
-        var serverSocket = new ServerSocket(portNumber);
-        var clientSocket = serverSocket.accept();
-        var out = new PrintWriter(clientSocket.getOutputStream(), true);
-        var in = new BufferedReader(
-            new InputStreamReader(clientSocket.getInputStream()))
-    ) {
-      out.write("HTTP/1.1 200 OK\r\n");
-      out.write("\r\n");
-      out.flush();
+  public void listen() {
+    try (var socket = new ServerSocket(portNumber)) {
+      while (true) new HttpServerHandler(socket.accept()).start();
     } catch (IOException e) {
-      System.out.println("Failed to start the server: " + e.getMessage());
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static class HttpServerHandler extends Thread {
+    private final Socket clientSocket;
+
+    public HttpServerHandler(Socket socket) {
+      this.clientSocket = socket;
+    }
+
+    public void run() {
+      try (var out = new PrintWriter(clientSocket.getOutputStream(), true);
+          var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+        try (clientSocket) {
+          out.write("HTTP/1.1 200 OK\r\n");
+          out.write("\r\n");
+          out.flush();
+        }
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
     }
   }
 }
